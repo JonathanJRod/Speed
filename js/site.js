646a8343812de9260e1946cfbf362207c6c9432d
JonathanJRod <j.hrodric@gmail.com>
2023-03-02 16:05:05 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,22 @@
-//Call HelloWorld() function
-function HelloWorld(){
-    alert("Hello World!");
+// alert("Hi from JS!");
+
+//get the values from the page
+//starts or controller function
+function getValues() {
+
+   
+}
+
+//generate numbers from startValue to the endValue
+//logic function(s)
+function generateNumbers() {
+
+
+}
+
+//display the numbers and mark the even numbers bold
+//display or view function(s)
+function displayNumbers() {
+
+
 }
